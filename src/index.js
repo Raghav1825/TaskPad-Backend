d21241fa318a48fd50f3c8091e15bc595c57e687
Raghav Arora <raghavarora9414@gmail.com>
2026-06-07 13:52:24 +0530

    implement user authentication, registration, and profile image uploads using Multer and Cloudinary

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
 import connnectDB from "./db/index.js";
-
+import {app} from "./app.js";
 
 dotenv.config({
     path:'./.env'
 })
 
 connnectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error on app: ",error);
+    })
+    app.listen(process.env.PORT || 5000 ,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Error connecting to MongoDB",error);
+})
